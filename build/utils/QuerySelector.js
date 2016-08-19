fddcf7f2104181ba69ec78a99f640bc43470a657
Pim Derks <info@pimderks.com>
2016-08-19 13:51:05 +0200

    Removed accidentally committed output directories.

--- build/utils/QuerySelector.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = function $$(selector) {
-    var context = arguments.length <= 1 || arguments[1] === undefined ? document : arguments[1];
-
-    return [].slice.call(context.querySelectorAll(selector));
-};
